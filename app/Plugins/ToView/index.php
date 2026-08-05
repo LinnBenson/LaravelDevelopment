65aa464bb99ec597963ada4b14c59e88761d032d
Linn Benson <bc@tobc.me>
2026-08-06 03:28:05 +0800

    更新

--- app/Plugins/ToView/index.php
+++ app/Plugins/ToView/index.php
@@ -16,7 +16,7 @@ return new class extends PluginProvider {
     public function __construct() {
         $this->name = 'To View';
         $this->description = 'System default view rendering component.';
-        $this->version = '1.0.1';
+        $this->version = '1.0.2';
         $this->author = 'System';
         $this->source = 'https://github.com/LinnBenson/LaravelDevelopment/releases/download/plugins-latest/ToView.zip';
     }
